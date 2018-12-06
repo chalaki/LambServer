48f4336645b227dcdbaf230dec710e13f7c185b4
Raja Sundarigari <sundarigari@gmail.com>
2018-12-06 09:36:42 +0530

    new4

--- survey-insert-2018-12-06T04.sql
+++ survey-insert-2018-12-06T04.sql
@@ -1,1 +1,2 @@
 INSERT into aa_survey_results (userid, ac_id, pb_id, a1, a2, user_timestamp) values (76844, 15, 77, 4, 11, '2018-12-06T02:45:14.600Z');
+INSERT into aa_survey_results (userid, ac_id, pb_id, a1, a2, user_timestamp) values (66648, 25, 89, 12, 9, '2018-12-06T04:00:33.907Z');
